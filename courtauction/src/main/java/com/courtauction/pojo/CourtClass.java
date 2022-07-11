58daa8878aecc58de95b0fe8b79cc4f13a67bfe2
eyabc <bey4314@naver.com>
2022-07-11 23:25:26 +0900

    #1 법원 분류 크롤링 추가

--- courtauction/src/main/java/com/courtauction/pojo/CourtClass.java
+++ courtauction/src/main/java/com/courtauction/pojo/CourtClass.java
@@ -2,8 +2,10 @@ package com.courtauction.pojo;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor(staticName = "of")
 public class CourtClass {
@@ -11,6 +13,6 @@ public class CourtClass {
     private final Set<String> courtClass;
 
     public static CourtClass of(List<String> courtClass) {
-        return CourtClass.of(courtClass.stream().toList());
+        return CourtClass.of(new HashSet<>(courtClass));
     }
 }
--- courtauction/src/main/java/com/courtauction/rest/CourtAuctionRest.java
+++ courtauction/src/main/java/com/courtauction/rest/CourtAuctionRest.java
@@ -25,10 +25,7 @@ public class CourtAuctionRest {
 
     public void fetchList() {
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Host", "ANDAR_API_APP_KEY");
-        headers.add("User-Agent", "ANDAR_API_APP_KEY");
-
+        HttpHeaders headers = getHttpHeaders();
         String uri = LIST_API + "?bubwLocGubun=1&jiwonNm=%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8&jpDeptCd=000000&daepyoSidoCd=&daepyoSiguCd=&daepyoDongCd=&notifyLoc=on&rd1Cd=&rd2Cd=&realVowel=35207_45207&rd3Rd4Cd=&notifyRealRoad=on&saYear=2022&saSer=&ipchalGbncd=&termStartDt=2022.07.04&termEndDt=2022.07.18&lclsUtilCd=&mclsUtilCd=&sclsUtilCd=&gamEvalAmtGuganMin=&gamEvalAmtGuganMax=&notifyMinMgakPrcMin=&notifyMinMgakPrcMax=&areaGuganMin=&areaGuganMax=&yuchalCntGuganMin=&yuchalCntGuganMax=&notifyMinMgakPrcRateMin=&notifyMinMgakPrcRateMax=&srchJogKindcd=&mvRealGbncd=00031R&srnID=PNO102001&_NAVI_CMD=&_NAVI_SRNID=&_SRCH_SRNID=PNO102001&_CUR_CMD=InitMulSrch.laf&_CUR_SRNID=PNO102001&_NEXT_CMD=RetrieveRealEstMulDetailList.laf&_NEXT_SRNID=PNO102002&_PRE_SRNID=&_LOGOUT_CHK=&_FORM_YN=Y";
         String body = restTemplate.postForEntity(uri,
                 new HttpEntity<>(headers),
@@ -42,9 +39,7 @@ public class CourtAuctionRest {
     }
 
     public CourtClass fetchCourtClass() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Host", "ANDAR_API_APP_KEY");
-        headers.add("User-Agent", "ANDAR_API_APP_KEY");
+        HttpHeaders headers = getHttpHeaders();
 
         String uri = OPTION_API + "?_NEXT_SRNID=PNO102001";
         String body = restTemplate.postForEntity(uri,
@@ -55,5 +50,13 @@ public class CourtAuctionRest {
                 .getElementById("idJiwonNm")
                 .select("option")
                 .eachText());
+    }
+
+    private HttpHeaders getHttpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Host", "www.courtauction.go.kr");
+        headers.add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36");
+        headers.add("Referer", "ttps://www.courtauction.go.kr/RetrieveRealEstMulDetailList.laf");
+        return headers;
     }
 }
